7cd621da82e851a31f2a662c20cc710338fa313a
Tadge O'Brien <49066408+er-obrien@users.noreply.github.com>
2024-05-15 12:57:24 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://er-obrien.github.io/microbit-control---forklift/](https://er-obrien.github.io/microbit-control---forklift/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/er-obrien/microbit-control---forklift** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/er-obrien/microbit-control---forklift** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,38 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    receivedString = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (receivedString == "up") {
+        pins.servoWritePin(AnalogPin.P0, 180)
+        pins.servoWritePin(AnalogPin.P1, 0)
+    }
+    if (receivedString == "down") {
+        pins.servoWritePin(AnalogPin.P0, 0)
+        pins.servoWritePin(AnalogPin.P1, 180)
+    }
+    if (receivedString == "right") {
+        pins.servoWritePin(AnalogPin.P0, 180)
+        pins.servoWritePin(AnalogPin.P1, 180)
+    }
+    if (receivedString == "left") {
+        pins.servoWritePin(AnalogPin.P0, 0)
+        pins.servoWritePin(AnalogPin.P1, 0)
+    }
+    if (receivedString == "stop") {
+        pins.servoWritePin(AnalogPin.P0, 90)
+        pins.servoWritePin(AnalogPin.P1, 90)
+    }
+    if (receivedString.charAt(0) == "x") {
+        pins.servoWritePin(AnalogPin.P2, parseFloat(receivedString.substr(1, 3)))
+    }
+    if (receivedString.charAt(0) == "c") {
+        pins.servoWritePin(AnalogPin.P2, parseFloat(receivedString.substr(1, 3)))
+    }
+})
+let receivedString = ""
+bluetooth.startUartService()
+basic.showIcon(IconNames.Square)
